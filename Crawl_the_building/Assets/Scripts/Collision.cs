30e6756d302c81851e7d1a230d23afa46f32934a
jiyullee <syk_09@naver.com>
2019-05-10 04:37:43 +0900

    shooting

---
+++ Crawl_the_building/Assets/Scripts/Collision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    public GameObject beforeWindow;
+    public GameObject afterWindow;
+   
+    void Start()
+    {
+        
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "bullet")
+        {     
+            afterWindow.SetActive(true);
+            beforeWindow.SetActive(false);     
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+   
+}
