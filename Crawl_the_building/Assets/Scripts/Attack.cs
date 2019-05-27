a84eb8045f2a0366e25e0cacf464cb7357ef6089
jiyullee <syk_09@naver.com>
2019-05-27 19:33:36 +0900

    Add raycast

--- Crawl_the_building/Assets/Scripts/Attack.cs
+++ Crawl_the_building/Assets/Scripts/Attack.cs
@@ -6,75 +6,32 @@ public class Attack : MonoBehaviour
 {
     public Vector3 mousePosition;
     public Transform firePosition;
-    [SerializeField]
-    Camera Camera;
-    [SerializeField] GameObject bullet;
-    [SerializeField] GameObject windowBullet;
+    public GameObject bullet;
+    public GameObject windowBullet;
     GameObject target;
     public int NumberOfBullet;
-    GameObject changer;
-    public int windowHP;
 
-    void Start()
+    public void ChooseShoot(GameObject targetObject)
     {
-       
-    }
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0) && NumberOfBullet > 0) //잔여총알 존재, 마우스 클릭시 실행
-        {           
-            CastRay(); 
-        }   
-    }
-
-    void CastRay() 
-    {
-        target = null;
-       
-        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-        RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero, 10f);
-
-        if (hit.collider != null && windowHP > 0)
+        target = targetObject;
+        if (target.tag == "Window")
         {
-            target = hit.collider.gameObject; //마우스가 위치한 타겟
-
-            if (target.tag == "Window") //타겟이 창문
-            {
-                target.tag = "targetWindow"; 
-                Debug.Log(target.tag);
-                windowAttack();
-                
-                NumberOfBullet--; //총알 개수 감소
-            }
-                        
-        }
-        else
-        {
-            attack();
-            NumberOfBullet--;
+           // ShootWindow();            
         }
        
-    }
-
-   void attack()
-    {   
-        mousePosition = Camera.ScreenToWorldPoint(Input.mousePosition);
+    }/*
+    void ShootWindow() 
+    {
+        mousePosition = GetComponent<Cam>().mousePosition;
         float dx = mousePosition.x - transform.position.x;
         float dy = mousePosition.y - transform.position.y;
         float angle = Mathf.Atan2(dx, dy) * Mathf.Rad2Deg;
         Quaternion Rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
         Instantiate(bullet, firePosition.position, Rotation);
-    }
-    void windowAttack()
+    }*/
+    void ShootEnemy()
     {
-        mousePosition = Camera.ScreenToWorldPoint(Input.mousePosition);
-        float dx = mousePosition.x - transform.position.x;
-        float dy = mousePosition.y - transform.position.y;
-        float angle = Mathf.Atan2(dx, dy) * Mathf.Rad2Deg;
-        Quaternion Rotation = Quaternion.Euler(new Vector3(0, 0, -angle));
-        Instantiate(windowBullet, firePosition.position, Rotation);
-    }
 
+    }
 
 }
---
+++ Crawl_the_building/Assets/Scripts/Cam.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public GameObject player;
+    Attack attack;
+    Camera cam;
+    GameObject target;
+    public Vector3 mousePosition;
+    float maxDistance = 15f;
+    void Start()
+    {
+        attack = player.GetComponent<Attack>();
+        mousePosition = Vector3.zero;
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = cam.ScreenToWorldPoint(mousePosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, transform.forward, maxDistance);
+            if (hit)
+            {
+                if (hit.collider != null)
+                {
+                    target = hit.collider.gameObject;
+                    attack.ChooseShoot(target);               
+                }
+            }
+                
+        }
+    }
+}
