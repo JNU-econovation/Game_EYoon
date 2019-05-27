54213126148e0d0864ca402e0d5d84979f372405
jedy100 <jedy100@naver.com>
2019-05-27 21:08:59 +0900

    add window hp

--- Crawl_the_building/Assets/Scripts/Bullet.cs
+++ Crawl_the_building/Assets/Scripts/Bullet.cs
@@ -5,13 +5,31 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     [SerializeField] float speed = 1;
+    GameObject target;
+    public Camera cam;
+    public int damage;
+    WindowHP windowhp;
     private void Start()
     {
-        Destroy(gameObject, 10f);
+        
+        target = cam.GetComponent<Cam>().target;
+        windowhp = target.GetComponent<WindowHP>();
+      
     }
     void Update()
     {
         transform.Translate(Vector3.up * speed);
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+        if (collider.gameObject == target)
+        {
+            windowhp.HP -= damage;
+            print(windowhp.HP);
+            Destroy(gameObject);
+            windowhp.ChangeWindow();
+        }
     }
 
 }
---
+++ Crawl_the_building/Assets/Scripts/WindowHP.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowHP : MonoBehaviour
+{
+    public int HP;
+    int maxHP;
+    int count = 0;
+    float temp = 0;
+    [SerializeField] Sprite[] brokenWindow;
+    SpriteRenderer sr;
+    private void Start()
+    {
+        maxHP = HP;
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+    public void ChangeWindow()
+    {
+
+        if (maxHP * (1 - temp - 0.25f) <= HP && HP < maxHP * (1 - temp))
+            sr.sprite = brokenWindow[count];
+        temp += 0.25f;
+        count++;
+
+
+    }
+
+}
