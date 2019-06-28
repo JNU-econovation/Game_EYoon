d3d72fc46ec4d67f2579a436358f382e20703311
jedy100 <jedy100@naver.com>
2019-06-28 15:44:37 +0900

    Add test UI

---
+++ Crawl_the_building/Assets/Scripts/Security.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Security : MonoBehaviour
+{
+    int delay = 3;
+    GameObject petrolWide;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    IEnumerator Petrol()
+    {
+        yield return new WaitForSeconds(delay);
+
+    }
+}
