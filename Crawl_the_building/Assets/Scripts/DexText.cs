a98c3b1ae4ed27c71f9979ff4f50f3dd540d90f8
jedy100 <jedy100@naver.com>
2019-07-03 11:59:35 +0900

    djdkjkdkfjdkfjdkfjkdfjdkfjk	 On branch master k

---
+++ Crawl_the_building/Assets/Scripts/DexText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DexText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PlayerPrefs.SetInt("Dex", 20);
+    }
+    private void Update()
+    {
+        GetComponent<Text>().text = "Dex: " + PlayerPrefs.GetInt("Dex");
+    }
+}
