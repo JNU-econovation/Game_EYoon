42255ae1f20f32c620097a2cbca677fe3c4a1a68
jedy100 <jedy100@naver.com>
2019-07-22 18:22:18 +0900

    Add lightning effect

---
+++ Crawl/Assets/Scripts/Lighning.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lighning : MonoBehaviour
+{
+   [SerializeField] GameObject player;
+    public int delay = 1;
+   [SerializeField] GameObject warning;
+   [SerializeField]
+    public float damege;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        StartCoroutine(LightningMake());
+    }
+
+    IEnumerator LightningMake()
+    {
+        GameObject siren = Instantiate(warning);
+        siren.transform.position = new Vector2(Random.Range(335, 385), player.transform.position.y + Random.Range(10, 80));
+        yield return new WaitForSeconds(delay);
+        GameObject temp = Instantiate(gameObject);
+        temp.transform.position = siren.transform.position;
+        Destroy(siren);
+        Destroy(temp, 1);
+    }
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<Health>().DecreaseHP(damege);
+        }
+    }
+}
--- Crawl/Assets/Scripts/LightningMaker.cs
+++ Crawl/Assets/Scripts/LightningMaker.cs
@@ -1,52 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LightningMaker : Weather
 {
-    public GameObject warning;
-    GameObject player;
+   
+    public GameObject lightning;
+    [SerializeField] GameObject flash;
     int numOfLight;
     int delay = 1;
     public float damege;
     
-    void Start()
-    {
-        player = GameObject.FindWithTag("Player");
-        StartCoroutine(LightningMake());
-    }
 
-    IEnumerator LightningMake()
-    {
-        
-        GameObject siren = Instantiate(warning);
-        siren.transform.position = new Vector2(Random.Range(335, 385), player.transform.position.y + Random.Range(10, 80));
-        yield return new WaitForSeconds(delay);
-        GameObject temp = Instantiate(gameObject);
-        temp.transform.position = siren.transform.position;     
-        Destroy(siren);
-        Destroy(temp, 1);
-    }  
 
     public override void MakeWeather()
     {
         numOfLight = Random.Range(1, 4);
         for(int i = 0; i < numOfLight; i++)
         {
-            Instantiate(gameObject);
+            Instantiate(lightning);
         }
+        StartCoroutine(LighningFlash());
+
     }
 
+    IEnumerator LighningFlash()
+    {
+        yield return new WaitForSeconds(lightning.GetComponent<Lighning>().delay);
+        float flashDelay = 0.4f;
+        flash.GetComponent<Animator>().SetBool("Flash", true);
+        yield return new WaitForSeconds(flashDelay);
+        flash.GetComponent<Animator>().SetBool("Flash", false);
+    }
     public override void Function()
     {
         
     }   
    
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-        if (collider.gameObject.tag == "Player")
-        {
-            collider.gameObject.GetComponent<Health>().DecreaseHP(damege);
-        }
-    }
+
 }
