5d33dcd1c49ef361db7b7585df7eae0fef8e0abc
jedy100 <jedy100@naver.com>
2019-07-26 02:40:35 +0900

    fix error

---
+++ Crawl/Assets/Scripts/nullweather.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nullweather : Weather
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public override void Function()
+    {
+        
+    }
+    public override void MakeWeather()
+    {
+     
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
