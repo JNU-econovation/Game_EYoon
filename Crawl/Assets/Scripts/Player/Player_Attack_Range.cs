e0c5053f4d1ab96c4ad60e95850e0170aba16f25
jiyullee <syk_09@naver.com>
2020-01-14 19:57:53 +0900

    Add range on/off button

--- Crawl/Assets/Scripts/Player/Player_Attack_Range.cs
+++ Crawl/Assets/Scripts/Player/Player_Attack_Range.cs
@@ -1,17 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Player_Attack_Range : MonoBehaviour
 {
     [SerializeField] float attackRange;
     float maxAttackRange = 15;
     Player_Circle_Move circle;
     public float tempRange;
+    SpriteRenderer spriteRenderer;
     void Start()
     {
         tempRange = attackRange;
         circle = GetComponentInChildren<Player_Circle_Move>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -38,5 +40,16 @@ public class Player_Attack_Range : MonoBehaviour
         attackRange += n;
         if (attackRange > maxAttackRange)
             attackRange = maxAttackRange;
+    }
+    public void ShowandHide_Circle()
+    {
+        if(spriteRenderer.color.a != 0)
+        {
+            spriteRenderer.color = new Color(255, 255, 255, 0);
+        }
+        else
+        {
+            spriteRenderer.color = new Color(255, 255, 255, 0.3f);
+        }
     }
 }
