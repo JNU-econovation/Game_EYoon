89ade87448826d18de15c125127d7ab4ce8f96bc
jiyullee <syk_09@naver.com>
2020-01-15 23:36:49 +0900

    Change font

--- Crawl/Assets/Scripts/BossWarning.cs
+++ Crawl/Assets/Scripts/BossWarning.cs
@@ -8,9 +8,11 @@ public class BossWarning : MonoBehaviour
     float r;
     float g;
     float b;
+    Color textColor;
     // Start is called before the first frame update
     private void OnEnable()
     {
+        textColor = GetComponentInChildren<Text>().color;
          r = GetComponent<Image>().color.r;
          g = GetComponent<Image>().color.g;
          b = GetComponent<Image>().color.b;
@@ -22,7 +24,7 @@ public class BossWarning : MonoBehaviour
         for(int i = 1; i <= 8; i++)
         {
             GetComponent<Image>().color = new Color(r, g, b, 0.1f*i );
-            GetComponentInChildren<Text>().color = new Color(1, 1, 1, 0.1f * (i+2));
+            GetComponentInChildren<Text>().color = new Color(textColor.r, textColor.g, textColor.b, 0.1f * (i+2));
             yield return new WaitForSeconds(0.05f);
         }
         yield return new WaitForSeconds (3.0f);
